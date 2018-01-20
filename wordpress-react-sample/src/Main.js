f89da48254a232fd785ab85620201c83436b5b64
Moncho Pena <moncho@rebellioncard.com>
2018-01-20 12:18:12 +0100

    Wordpress React Sample Base

---
+++ wordpress-react-sample/src/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import About from './About'
+import Home from './Home'
+import Post from './Post'
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route path='/:slug' component={Post} />
+        </Switch>
+      </main>
+    );
+  }
+}
+
+export default Main
