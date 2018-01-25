c0692cfe003a1f436b6043072c858a5a697870a3
Moncho Pena <moncho@rebellioncard.com>
2018-01-25 11:52:44 +0100

    Mostrando las imágenes

--- wordpress-react-sample/src/Post.js
+++ wordpress-react-sample/src/Post.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import shortid from 'shortid'
+import random from 'lodash/random'
 
 const postStyle = {
   'width': '100%'
 }
 
+const images = [
+  'cherries.jpg'
+  , 'croissant.jpg'
+  // , 'gondol.jpg'
+  , 'popsicle.jpg'
+  , 'salmon.jpg'
+  , 'sandwich.jpg'
+  , 'steak.jpg'
+  , 'wine.jpg'
+  // , 'workshop.jpg'
+]
+
 class Post extends Component {
   render() {
-    const now = shortid.generate()
+    const randomNumber = random(images.length -1)
+    const image = images[randomNumber]
     return (
       <div className="w3-quarter">
-        <img src={"https://loremflickr.com/g/447/675/kitties?rand="+now} alt="{this.props.slug}" style={postStyle} />
+        <img src={"img/"+image} alt="{this.props.slug}" style={postStyle} />
         <h3><Link to={`/${this.props.slug}`}>{this.props.title}</Link></h3>
       </div>
     );
